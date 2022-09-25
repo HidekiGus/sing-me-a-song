3d7c9f4c91d6b44dd7479b3dddadc4c0a72f7391
Gustavo <gustavohideki@outlook.com>
2022-09-25 04:08:36 -0300

    Feat: add env variable for test mode

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -1,5 +1,6 @@
 import cors from 'cors';
 import express from 'express';
+import dotenv from 'dotenv';
 import 'express-async-errors';
 import { errorHandlerMiddleware } from './middlewares/errorHandlerMiddleware.js';
 import recommendationRouter from './routers/recommendationRouter.js';
@@ -10,7 +11,9 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/recommendations', recommendationRouter);
-app.use(testsRouter);
+if (process.env.NODE_ENV === 'test') {
+  app.use(testsRouter);
+}
 app.use(errorHandlerMiddleware);
 
 export default app;
